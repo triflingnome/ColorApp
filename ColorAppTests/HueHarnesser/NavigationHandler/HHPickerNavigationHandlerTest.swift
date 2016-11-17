4c1be6d967f20ac6a150a9a0b3fc5e2b0ecfc4c2
triflingnome <michael.rooney.berg@gmail.com>
2016-11-17 13:37:41 -0600

    Navigation Handlers
    - built out HHPickerNavigationHandlerTest

--- ColorAppTests/HueHarnesser/NavigationHandler/HHPickerNavigationHandlerTest.swift
+++ ColorAppTests/HueHarnesser/NavigationHandler/HHPickerNavigationHandlerTest.swift
@@ -5,3 +5,31 @@
 //  Created by Michael Berg on 11/17/16.
 //  Copyright © 2016 Michael Berg. All rights reserved.
 //
+
+import XCTest
+
+@testable import ColorApp
+
+class HHPickerNavigationHandlerTest: XCTestCase {
+    
+    func testHandleNavigation() {
+        let navigationController = UINavigationController()
+        let animated = true
+        let storyboardName = Bundle.main.infoDictionary?["UIMainStoryboardFile"] as! String?
+        
+        if let actualStoryboardName = storyboardName {
+            let storyboard = UIStoryboard(name: actualStoryboardName, bundle: Bundle.main)
+
+            let pickerNavigationHandler = HHPickerNavigationHandler(navigationController: navigationController, animated: animated, storyboard: storyboard)
+            pickerNavigationHandler.handleNavigation()
+            
+            let viewControllers = navigationController.viewControllers
+            guard viewControllers[viewControllers.endIndex - 1] is ColorPickerViewController else {
+                XCTFail("top view controller is not of type ColorPickerViewController")
+            }
+        } else {
+            XCTFail("main.storyboard or debug image not found")
+        }
+    }
+    
+}
