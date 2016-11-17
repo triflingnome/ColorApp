6288cd858d3b6e28c871910febfbebe39d70d663
triflingnome <michael.rooney.berg@gmail.com>
2016-11-16 19:45:09 -0600

    Navigation Handlers
    - built out HHExtractorNavigationHandler

--- HueHarnesser/NavigationHandler/HHExtractorNavigationHandler.swift
+++ HueHarnesser/NavigationHandler/HHExtractorNavigationHandler.swift
@@ -5,3 +5,28 @@
 //  Created by Michael Berg on 11/16/16.
 //  Copyright © 2016 Michael Berg. All rights reserved.
 //
+
+import UIKit
+
+class HHExtractorNavigationHandler: HHNavigationHandleable {
+    
+    let navigationController: UINavigationController
+    let animated: Bool
+    let image: UIImage
+    let storyboard: UIStoryboard // will be removed once ColorExtractorVC is rewritten to not use storyboard
+    
+    init(navigationController: UINavigationController, animated: Bool, image: UIImage, storyboard: UIStoryboard) {
+        self.navigationController = navigationController
+        self.animated = animated
+        self.image = image
+        self.storyboard = storyboard
+    }
+    
+    func handleNavigation() {
+        let colorExtractorViewController = storyboard.instantiateViewController(withIdentifier: "ColorExtractorViewController") as! ColorExtractorViewController
+        colorExtractorViewController.chosenImage = image
+        
+        navigationController.pushViewController(colorExtractorViewController, animated: animated)
+    }
+    
+}
