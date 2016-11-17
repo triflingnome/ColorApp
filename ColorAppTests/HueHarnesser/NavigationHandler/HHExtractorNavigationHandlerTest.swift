ed5c11a100bb339f8a8a8a19fd5c90c6f6e0145f
triflingnome <michael.rooney.berg@gmail.com>
2016-11-17 12:20:34 -0600

    Navigation Handler
    - further configuration of unit testing suite for project

--- ColorAppTests/HueHarnesser/NavigationHandler/HHExtractorNavigationHandlerTest.swift
+++ ColorAppTests/HueHarnesser/NavigationHandler/HHExtractorNavigationHandlerTest.swift
@@ -5,3 +5,21 @@
 //  Created by Michael Berg on 11/17/16.
 //  Copyright © 2016 Michael Berg. All rights reserved.
 //
+
+import XCTest
+
+@testable import ColorApp
+
+class HHExtractorNavigationHandlerTest: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        
+        // TODO: set up instance of UINavigationController here
+    }
+    
+    func testHandleNavigation() {
+        XCTAssertTrue(true)
+    }
+    
+}
