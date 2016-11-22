de6ec638276e3ceeac78f6fa53b8f13491839574
triflingnome <michael.rooney.berg@gmail.com>
2016-11-21 20:19:54 -0600

    Refactor CoreData
    - got Xcode to autogenerate the HHHueMO model, which is a OO version of the Hue entity in the Saved Hues managed object model
    - started building out HHDataManager

--- HueHarnesser/Manager/HHDataManager.swift
+++ HueHarnesser/Manager/HHDataManager.swift
@@ -5,3 +5,48 @@
 //  Created by Michael Berg on 11/21/16.
 //  Copyright © 2016 Michael Berg. All rights reserved.
 //
+
+import CoreData
+
+@objc class HHDataManager: NSObject {
+    
+    // MARK: Properties
+    
+    // TODO: NSFetchedResultsController computed property
+    var fetchedResultsController: NSFetchedResultsController<HHHueMO> {
+//        if _fetchedResultsController != nil {
+//            return _fetchedResultsController!
+//        }
+        
+        let fetchRequest: NSFetchRequest<HHHueMO> = HHHueMO.fetchRequest()
+        let sortDescriptor = NSSortDescriptor(key: "name", ascending: false)
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let managedObjectContext = appDelegate.persistentContainer.viewContext
+        let aFetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest,
+                                                                   managedObjectContext: managedObjectContext,
+                                                                   sectionNameKeyPath: nil,
+                                                                   cacheName: "DataManager")
+//        aFetchedResultsController.delegate = self
+//        _fetchedResultsController = aFetchedResultsController
+        
+        do {
+            try aFetchedResultsController.performFetch()
+        } catch {
+            let error = error as NSError
+            fatalError("Unresolved error \(error), \(error.userInfo)")
+        }
+        
+        return aFetchedResultsController
+    }
+    
+//    var _fetchedResultsController: NSFetchedResultsController<HHHueMO>? = nil
+    
+    // MARK: Public Methods
+    
+    // TODO: class func saveHue(with hue: HHHueMO) { }
+    
+    // TODO: class func deleteHue(with hue: HHHueMO) { }
+    
+}
