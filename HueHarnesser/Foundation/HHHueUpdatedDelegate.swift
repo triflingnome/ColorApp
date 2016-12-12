990fa0a0b301ff2835e4ab4f97ba544bec93fc63
triflingnome <michael.rooney.berg@gmail.com>
2016-12-12 17:57:46 -0600

    1.2 - HHHueUpdatedDelegate
    - added HHHueUpdatedDelegate protocol to file
    - added updated(with hue: UIColor) instance method to HHHueUpdatedDelegate protocol

--- HueHarnesser/Foundation/HHHueUpdatedDelegate.swift
+++ HueHarnesser/Foundation/HHHueUpdatedDelegate.swift
@@ -5,3 +5,15 @@
 //  Created by Michael Berg on 12/12/16.
 //  Copyright © 2016 Michael Berg. All rights reserved.
 //
+
+/**
+ * This delegate provides a set of methods that a given
+ * class can conform to, to provide updated color information
+ * to its delegate
+ */
+protocol HHHueUpdatedDelegate {
+    
+    /// This instance method provides updated color information in the form of a UIColor object
+    func updated(with hue: UIColor)
+    
+}
